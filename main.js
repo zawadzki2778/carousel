be4d14e0c418495b89bd625889865df1a609d580
zawadzki2778 <zawadzki2778@gmail.com>
2022-08-19 15:37:11 +0300

    записал логику работы прокрутки + код для правой стрелки и ее возврата

--- main.js
+++ main.js
@@ -7,26 +7,18 @@ window.addEventListener('load', () => {
   const images = document.querySelectorAll('.carousel__slide img');//получаем все картинки
   // счётчики 
   let counter = 0;
-  const stepSize = images[0].clientWidth;// узнаём размер области внутри рамок элемента что бы получить шаг прокрутки 
+  const stepSize = images[0].clientWidth;// узнаём размер области внутри рамок элемента[0],что бы получить шаг прокрутки 
   console.log(stepSize);
 
+  //записываем логику работы прокрутки 
+  slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+  //пишем код реализацмии для правой стрелки
+  right.addEventListener('click', () => {
+    //прописываем условие для конечной точки прокрутки и возврата её в начало 
+    if (counter >= images.length - 1) counter = -1;
+    slider.classList.add('transformAnimation');// добавили класс, прописанный в style.css
+    counter++;
+    slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
+  });
+});
