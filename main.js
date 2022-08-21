e3b98291dab87082108a64c2ffa3a3d3b279e6eb
zawadzki2778 <zawadzki2778@gmail.com>
2022-08-21 16:17:06 +0300

    разношу кнопки по краям слайдера(в процессе...)

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ window.addEventListener('load', () => {
   //кнопки
   const left = document.querySelector('.btn-left');
   const right = document.querySelector('.btn-right');
+
   //carousel
   const slider = document.querySelector('.carousel__slide');
   const images = document.querySelectorAll('.carousel__slide img');//получаем все картинки
+
   // счётчики 
   let counter = 0;
-  const stepSize = images[2].clientWidth;// узнаём размер области внутри рамок элемента[0],что бы получить шаг прокрутки 
+  const stepSize = images[0].clientWidth;// узнаём размер области внутри рамок элемента[0],что бы получить шаг прокрутки 
   console.log(stepSize);
 
   //записываем логику работы прокрутки 
@@ -30,3 +32,5 @@ window.addEventListener('load', () => {
     slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
   });
 });
+
+
