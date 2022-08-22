ab6377112af9e77f3283e595786113d9348c0834
zawadzki2778 <zawadzki2778@gmail.com>
2022-08-22 14:15:24 +0300

    добавил папку с новыми фото, добавил код для адаптива)

--- main.js
+++ main.js
@@ -1,36 +1,54 @@
-window.addEventListener('load', () => {
-  //кнопки
-  const left = document.querySelector('.btn-left');
-  const right = document.querySelector('.btn-right');
+//кнопки
+const left = document.querySelector('.btn-left');
+const right = document.querySelector('.btn-right');
+//carousel
+const slider = document.querySelector('.carousel__slide');
+const images = document.querySelectorAll('.carousel__slide .carousel__img');//получаем все картинки
+console.log(images);
+// счётчики 
+let counter = 0;
+let width;
 
-  //carousel
-  const slider = document.querySelector('.carousel__slide');
-  const images = document.querySelectorAll('.carousel__slide img');//получаем все картинки
+function size() {
+  console.log('resize');
+  width = document.querySelector('.carousel__container').offsetWidth;// узнаём ширину блока carousel__container
+  console.log(width);
 
-  // счётчики 
-  let counter = 0;
-  const stepSize = images[0].clientWidth;// узнаём размер области внутри рамок элемента[0],что бы получить шаг прокрутки 
-  console.log(stepSize);
-
-  //записываем логику работы прокрутки 
-  slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
-
-  //пишем код реализацмии для правой стрелки
-  right.addEventListener('click', () => {
-    //прописываем условие для конечной точки прокрутки и возврата её в начало 
-    if (counter >= images.length - 1) counter = -1;
-    slider.classList.add('transformAnimation');// добавили класс, прописанный в style.css
-    counter++;
-    slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
+  slider.style.width = width * images.length + 'px';
+  images.forEach(item => {
+    item.style.width = width + 'px';
+    item.style.width = 'auto'; //принудительно ставим высоту картинок авто 
   });
-  /// для левой стрелки
-  left.addEventListener('click', () => {
-    //
-    if (counter <= 0) counter = images.length;
-    slider.classList.add('transformAnimation');// добавили класс, прописанный в style.css
-    counter--;
-    slider.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
-  });
+  rollSlider();
+}
+window.addEventListener('resize', size);
+size();
+
+function rollSlider() {
+  slider.style.transform = 'translate(-' + counter * width + 'px)';
+}
+
+//пишем код реализацмии для правой стрелки
+right.addEventListener('click', () => {
+  //прописываем условие для конечной точки прокрутки и возврата её в начало 
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+  slider.classList.add('transformAnimation');// добавили класс, прописанный в style.css
+  rollSlider();
 });
+
+// для левой стрелки
+left.addEventListener('click', () => {
+  //
+  counter--;
+  if (counter < 0) {
+    counter = images.length - 1;
+  }
+  slider.classList.add('transformAnimation');// добавили класс, прописанный в style.css
+  rollSlider();
+});
+
 
 
